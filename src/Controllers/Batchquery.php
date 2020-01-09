227884f08fe147be843bcba602a716758978173c
rajmn <49806251+rajmn@users.noreply.github.com>
2020-01-09 18:18:15 +0530

    Update Batchquery.php

--- src/Controllers/Batchquery.php
+++ src/Controllers/Batchquery.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Controller;
 
 class Batchquery extends Controller
 {
-    public function batchUpdate(Request $request,$cIndex='')
+    public function batchUpdate($cIndex='')
     {   die($cIndex);
         $caseVal = [];
         $setP    = [];
